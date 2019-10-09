249c5f5be19282d622ce19ac051ec7685d7346b4
Christian Perez <perezchristian1012@gmail.com>
2019-10-09 01:19:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,47 +1,32 @@
 class Artist
-  attr_accessor :name
-  attr_reader :songs
-  @@all = []
 
-  def initialize(name)
+  attr_accessor :name # Create our attribute for the artist's name.
+
+  @@all = [] # Create a global variable to store all our our initialized artists.
+
+  def initialize(name) # First, we initialize our Artist class with the name attribute.
     @name = name
-    @songs = []
-    save
+    @@all << self # We take our class instance and push it into our class variable.
   end
 
-  def self.all
+  def self.all # A class method that will return the contents of our class variable '@@all'
     @@all
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def songs # Second, we create our 'songs' method for returning all songs associated with a specific artist.
+    Song.all.select {|song| song.artist == self} # We access our 'Song' class and its class variable and use 'select' on it.
+  end # We then set the paramenters to return an array of all the songs whose artist is equal to the instance of the class.
+
+  def add_song(song) # Third, we create 'add_song' to associate a song name with a particular artist.
+    song.artist = self # When we call 'song.artist', we set the result equal to 'self', or the artist on which we are calling the method.
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
-
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) || self.create(name)
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
+  def self.find_or_create_by_name(artist_name) # Fourth, we take our class, the '.all' method,and then look through our data for a name that equals our artist's name.
+    self.all.find {|artist_info| artist_info.name == artist_name} || self.new(artist_name)
   end
 
   def print_songs
-    songs.each { |song| puts song.name }
-  end
-
-  def save
-   @@all << self
+    self.songs.each {|song| puts song.name}
   end
 
 end
